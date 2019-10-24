5385850f7655c30cf1eea164cf66295e8252850c
lisannec12 <56953083+lisannec12@users.noreply.github.com>
2019-10-24 09:05:30 +0200

    Add files via upload

---
+++ Maatschappijleer folder/Index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Maatschappijleer </title>
+		<link rel="stylesheet" href="stylesheet.css" type="text/css">
+	</head>
+	<body>
+		<form action="process.php" method="POST">
+			hoi doe ff je naam<input type="text" name="name">
+			<input type="submit">
+		</form>
+			<h1> Klik NU op een van de bestanden. </h1>
+				<ul> 
+					<li> <a href="mindmap.png"> Mindmap </a> </li>
+					<li> <a href="https://docs.google.com/document/d/1Mt6IaqfZ6pwYKAZ5jm0oiZTbr8QBNVhxFlEUG0lP508/edit"> Zorg document </a> </li>
+					<li> <a href="http://culturescope.nl/gezondheidszorg-en-volksgezondheid-10/"> Bron lux </a> </li>
+				</ul>
+			<img src="maartje.jpeg" height="400px" width="600px">
+				<p> Maartje van der Pluijm als Elsa.</p>
+			<img src="lex.jpg" height="400px" width="600px">
+				<p>wauw</p>
+			<img src="yello.jpg" height="400px" width="600px">
+				<p> leuk dat je dit leest. </p>
+	</body>
+</html>
